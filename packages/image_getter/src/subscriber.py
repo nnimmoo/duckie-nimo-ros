d58311006c77f5ab1a1dc66fd2a2b8faeb7d8565
Nimo <ninoguadze@gmail.com>
2023-06-01 13:53:54 +0400

    add duckie_detector package

--- packages/image_getter/src/subscriber.py
+++ packages/image_getter/src/subscriber.py
@@ -15,24 +15,18 @@ class MySubscriberNode(DTROS):
         super(MySubscriberNode, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
         # construct publisher
         #params:
-        self.image = None
-        self.br = CvBridge()
-        self.num = 0
+        #self.image = None
+        #self.br = CvBridge()
+        #self.num = 0
         # Node cycle rate (in Hz).
-        self.loop_rate = rospy.Rate(1)
+        #self.loop_rate = rospy.Rate(1)
         #self.sub = rospy.Subscriber('/'+ os.environ['DUCKIEBOT_NAME'] + "/camera_node/image/raw", Image, self.Imagecallback)
         #self.sub = rospy.Subscriber("/camera/image",Image,self.callback)
         self.sub = rospy.Subscriber('chatter', String, self.callback)
 
     def callback(self, data):
-        img = np.random.randint(255, size=(300, 600, 3))
         rospy.loginfo('Image received...')
         #self.image = self.br.imgmsg_to_cv2(msg)
-        if not cv2.imwrite("saved"+str(self.num)+".jpg", img):
-            raise Exception("Could not write image")
-        cv2.imshow('image',img)
-        cv2.waitKey(0)
-        self.num += 1
 
 if __name__ == '__main__':
     # create the node
